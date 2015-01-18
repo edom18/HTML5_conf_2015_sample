e1ef6c0f9c4244a3916b13c07243e7da79958922
edom18 <edo.m18@gmail.com>
2015-01-18 22:49:08 +0900

    面からキューブの生成

--- js/main.js
+++ js/main.js
@@ -5,35 +5,109 @@
         Detector.addGetWebGLMessage();
     }
 
-    var hash = document.location.hash.substr(1);
-    if (hash) {
-        hash = parseInt(hash, 0);
-    }
+    var PI_2 = Math.PI / 2;
 
-    /* TEXTURE WIDTH FOR SIMULATION */
-    var WIDTH = hash || 32;
+    var domContainer;
 
-    var container;
-
-    var object,
+    var container,
         camera,
         light,
         scene,
         renderer;
 
-    var geometry,
-        material,
-        mesh;
+    var cube;
+    var plane;
+
+    function createCube(width, height, depth) {
+        var segments = 10;
+        var faceGeometry = new THREE.PlaneBufferGeometry(width, height, segments, segments);
+        var faceMaterial = new THREE.MeshLambertMaterial({ color: 0xaa3333 });
+
+        function createFace(width, height) {
+            var halfWidth  = width / 2;
+            var halfHeight = height / 2;
+
+            var prevFace;
+
+            var joints = [],
+                facies = [];
+            for (var i = 0; i < 4; i++) {
+                var joint = new THREE.Object3D();
+                var face  = new THREE.Mesh(faceGeometry, faceMaterial);
+
+                if (i !== 0) {
+                    joint.position.x = halfWidth;
+                    face.position.x  = halfWidth;
+                }
+                joint.add(face);
+                if (prevFace) {
+                    prevFace.add(joint);
+                }
+
+                joints.push(joint);
+                facies.push(face);
+
+                prevFace = face;
+            }
+
+            return {
+                joints: joints,
+                facies: facies
+            };
+        }
+
+
+        var topFace    = new THREE.Mesh(faceGeometry, faceMaterial);
+        topFace.castShadow = true;
+        var bottomFace = new THREE.Mesh(faceGeometry, faceMaterial);
+        bottomFace.castShadow = true;
+
+        var topJoint    = new THREE.Object3D();
+        topJoint.add(topFace);
+        var bottomJoint = new THREE.Object3D();
+        bottomJoint.add(bottomFace);
+
+        var halfWidth  = width / 2;
+        var halfHeight = height / 2;
+
+        topJoint.position.y = halfHeight;
+        topFace.position.y  = halfHeight;
+
+        bottomJoint.position.y = -halfHeight;
+        bottomFace.position.y  = -halfHeight;
+
+        topJoint.rotation.x    = -PI_2;
+        bottomJoint.rotation.x = PI_2;
+
+
+        var ret = createFace(width, height);
+        ret.facies[2].add(topJoint);
+        ret.facies[2].add(bottomJoint);
+
+        ret.joints.forEach(function (joint, i) {
+            joint.rotation.y = PI_2;
+        });
+        ret.facies.forEach(function (mesh, i) {
+            mesh.castShadow = true;
+        });
+
+        var mainJoint = ret.joints[0];
+        mainJoint.position.x = halfWidth;
+
+        var cube = new THREE.Object3D();
+        cube.add(mainJoint);
+
+        return cube;
+    }
 
     function init() {
 
-        container = document.createElement('div');
-        document.body.appendChild(container);
-
-        //
+        domContainer = document.createElement('div');
+        document.body.appendChild(domContainer);
 
         // オブジェクト（箱）を生成
-        object = new THREE.Object3D();
+        container = new THREE.Object3D();
+        container.position.y = 70;
 
         // カメラを生成
         var fov    = 75;
@@ -41,32 +115,49 @@
         var zNear  = 1;
         var zFar   = 3000;
         camera = new THREE.PerspectiveCamera(fov, aspect, zNear, zFar);
+        camera.position.y = 100;
         camera.position.z = 150;
 
         // ライトを生成
         light = new THREE.DirectionalLight(0xffffff);
-        light.position.set(100, 10, 100);
+        light.position.set(100, 100, 100);
+        light.castShadow = true;
+        light.shadowMapWidth  = 2048;
+        light.shadowMapHeight = 2048;
+
+        // light.shadowCameraVisible = true;
 
         // シーンを生成
         scene = new THREE.Scene();
 
-        geometry = new THREE.BoxGeometry(50, 50, 50);
-        material = new THREE.MeshLambertMaterial({
-            color: 0xaa0000
-        });
-        mesh = new THREE.Mesh(geometry, material);
+        cube = createCube(50, 50, 50);
+        cube.castShadow = true;
+        container.add(cube);
 
-        scene.add(mesh);
+        //
+
+        var planeGeometry = new THREE.PlaneBufferGeometry(1000, 1000, 1000);
+        var planeMaterial = new THREE.MeshLambertMaterial({
+            color: 0xdddddd
+        });
+        plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+        plane.rotation.x    = -PI_2;
+        plane.receiveShadow = true;
+
+        scene.add(plane);
+        scene.add(container);
         scene.add(light);
 
-        camera.lookAt(mesh.position);
+        camera.lookAt(container.position);
 
         // レンダラーを生成
         renderer = new THREE.WebGLRenderer();
-        renderer.setClearColor(0x000000);
+        renderer.setClearColor(0xffffff);
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
-        container.appendChild(renderer.domElement);
+        renderer.shadowMapEnabled = true;
+        domContainer.appendChild(renderer.domElement);
     }
 
     init();
@@ -74,7 +165,8 @@
     (function animate() {
         requestAnimationFrame(animate);
 
-        mesh.rotation.x += 0.01;
+        container.rotation.x += 0.01;
+        container.rotation.y += 0.005;
 
         render();
     }());
